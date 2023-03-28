95686a83072f20c763ea0f653ced6526ac271f26
AndreNeves30 <andre.neves30@etec.sp.gov.br>
2023-03-28 20:21:38 -0300

    AAAAAAAAAAA

---
+++ app/src/main/java/br/com/etecia/smartcidadesapp/SplashActivity.java
@@ -0,0 +1,31 @@
+package br.com.etecia.smartcidadesapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SplashActivity extends AppCompatActivity {
+
+Button btnentrar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_layout);
+
+        btnentrar = findViewById(R.id.btnentrarSplash);
+
+
+        btnentrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),
+                LoginActivity.class));
+                finish();
+            }
+        });
+    }
+}
